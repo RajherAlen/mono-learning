ba688e35e58705f35534471116125f11db65db4b
Alen <alen.rajher@gmail.com>
2021-05-20 19:08:40 +0200

    split app functions and add in index.js

--- src/app/app.js
+++ src/app/app.js
@@ -1,19 +1,15 @@
-import { Actions } from "./utils/actions";
 import Duration from "./utils/duration";
-
-const trafficLightVertical = $(".traffic-lights--vertical");
-const trafficLightHorizontal = $(".traffic-lights--horizontal");
 
 const duration = new Duration(6000, 1000, 1000, 1500);
 
-const actionsHorizontal = new Actions(trafficLightHorizontal);
-const actionsVertical = new Actions(trafficLightVertical);
-
 class Traffic {
-	constructor() {}
+	constructor(horizontal, vertical) {
+		this.horizontalFunc = horizontal;
+		this.verticalFunc = vertical;
+	}
 
 	horizontal() {
-		actionsHorizontal.activateLights();
+		this.horizontalFunc.activateLights();
 
 		setTimeout(() => {
 			this.vertical();
@@ -21,7 +17,7 @@ class Traffic {
 	}
 
 	vertical() {
-		actionsVertical.activateLights();
+		this.verticalFunc.activateLights();
 
 		setTimeout(() => {
 			this.horizontal();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+import { Actions } from "./app/utils/actions";
 import Traffic from "./app/app";
 
-const newTraffic = new Traffic();
+const trafficLightVertical = $(".traffic-lights--vertical");
+const trafficLightHorizontal = $(".traffic-lights--horizontal");
+
+const actionsHorizontal = new Actions(trafficLightHorizontal);
+const actionsVertical = new Actions(trafficLightVertical);
+
+const newTraffic = new Traffic(actionsHorizontal, actionsVertical);
 
 (function () {
 	newTraffic.vertical();
