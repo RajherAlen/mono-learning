7be5cf1637cfd35fbb3b812e85accbfd612d53ed
Alen Rajher <alen.rajher@gmail.com>
2021-05-23 09:35:27 +0200

    make private

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import { Actions } from "./app/utils/actions";
 import Traffic from "./app/app";
 
-const trafficLightVertical = $(".traffic-lights--vertical");
-const trafficLightHorizontal = $(".traffic-lights--horizontal");
-
-const actionsHorizontal = new Actions(trafficLightHorizontal);
-const actionsVertical = new Actions(trafficLightVertical);
-
-const newTraffic = new Traffic(actionsHorizontal, actionsVertical);
-
 (function () {
-	newTraffic.vertical();
+    const trafficLightVertical = $(".traffic-lights--vertical");
+    const trafficLightHorizontal = $(".traffic-lights--horizontal");
+
+    const actionsHorizontal = new Actions(trafficLightHorizontal);
+    const actionsVertical = new Actions(trafficLightVertical);
+
+    const newTraffic = new Traffic(actionsHorizontal, actionsVertical);
+
+    newTraffic.vertical();
 })();
